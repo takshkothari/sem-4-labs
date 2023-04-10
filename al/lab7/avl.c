751583c3d695b4acefbf50cbe7d21073d8ad7ee0
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2023-04-10 16:25:30 +0530

    avl fixed

--- al/lab7/avl.c
+++ al/lab7/avl.c
@@ -12,39 +12,6 @@ Nodeptr getnode() {
     return ((Nodeptr)malloc(sizeof(struct node)));
 }
 
-Nodeptr Insert_BST(Nodeptr root, int item) {
-    if (!root) {
-        root = getnode();
-        root->data= item;
-        root->lchild=root->rchild = NULL;
-        return root;
-    }
-    else {
-        if (item<root->data)
-            root->lchild = Insert_BST(root->lchild, item);
-        else if (item>root->data)
-            root->rchild = Insert_BST(root->rchild, item);
-        else
-            printf("Duplicates are not allowed\n");
-        return root;
-    }
-}
-
-Nodeptr create_BST(Nodeptr root,int data) {
-    int x;
-    root=getnode();
-    root->data=data;
-    root->lchild=root->rchild=NULL;
-    while(1) {
-        printf("Enter data to insert (-1 to exit): ");
-        scanf("%d",&x);
-        if(x==-1)
-            break;
-        root=Insert_BST(root,x);
-    }
-    return root;
-}
-
 int max(int a,int b) {
     return a>b?a:b;
 }
@@ -118,7 +85,7 @@ Nodeptr create_AVL(Nodeptr root,int data) {
     root->data=data;
     root->lchild=root->rchild=NULL;
     while(1) {
-        printf("Enter data to insert (-1 to exit): ");
+        printf("enter element: ");
         scanf("%d",&x);
         if(x==-1)
             break;
@@ -138,7 +105,7 @@ void preorder(Nodeptr root) {
 void main() {
     Nodeptr root=NULL;
     int data;
-    printf("Enter Root Data: ");
+    printf("enter root: ");
     scanf("%d",&data);
     root=create_AVL(root,data);
     printf("\n");
