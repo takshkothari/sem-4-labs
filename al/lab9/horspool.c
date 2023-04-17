17951fa1a549e16386ecd727d0efa88a407f10aa
Taksh Kothari <96124613+takshkothari@users.noreply.github.com>
2023-04-17 15:33:18 +0530

    horspool

--- al/lab9/horspool.c
+++ al/lab9/horspool.c
@@ -1,48 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int t[100];
+void getShiftTable(int shiftTable[], char pattern[], int pLen) {
+    int i;
+    for(i = 0; i < pLen - 1; i++)
+        shiftTable[(int) pattern[i]] = pLen -1 - i;
+}
 
-void shifttable(char p[]){
-    int i, j, m;
-    m = strlen(p);
-    for(i = 0 ; i < 100 ; i++)
-        t[i] = m;
-    for(j = 0 ; j < m-1 ; j++)
-        t[p[j]] = m - 1 - j;
-}
-int horspool(char src[],char p[]){
-    int i, j, k, m, n;
-    n = strlen(src);
-    m = strlen(p);
-    printf("Length of the text = %d \n",n);
-    printf("Length of the pattern = %d \n",m);
-    i = m - 1;
-    while(i < n) { 
-        k = 0;
-        while((k < m) && (p[m-1-k] == src[i-k]))
-            k++;
-    if(k == m)
-        return(i - m + 1);
-    else
-        i += t[src[i]];
+int horspool(char sentence[], char pattern[], int sLen, int pLen, int *comparisons) {
+    int i, j;
+    int shiftTable[127];
+    for (i = 0; i < 127; i++)
+        shiftTable[i] = pLen;
+    getShiftTable(shiftTable,pattern,pLen);
+    i  = pLen - 1;
+    while (i < sLen) {
+        j = 0;
+        while (j < pLen && pattern[pLen - 1 - j] == sentence[i - j])
+            j++;
+        *comparisons = *comparisons + j + 1;
+        if (j == pLen)
+            return (i - pLen + 1);
+        else 
+            i = i + shiftTable[(int) sentence[i]];
     }
-  return -1;
+    return -1;
 }
-int main()
-{
-    char src[100], p[100];
-    int pos;
-    printf("Enter the text : \n");
-    gets(src);
-    printf("Enter the pattern : \n");
-    gets(p);
-    shifttable(p);
-    pos = horspool(src, p);
-    if(pos >= 0)
-        printf("The desired pattern was found starting from position %d \n",pos+1);
+
+void main() {
+    int res, comparisons = 0;;
+    char sentence[100], pattern[100];
+    printf("Enter the sentence ");
+    scanf("%[^\n]",sentence);
+    printf("Enter the pattern ");
+    scanf(" %s",pattern);
+    res = horspool(sentence, pattern, strlen(sentence), strlen(pattern), &comparisons);
+    if (res == -1)
+        printf("Pattern not found. Comparisons: %d\n",comparisons);
     else
-        printf("The pattern was not found in the given text \n");
-    return 0;
+        printf("Pattern found at index %d. Comparisons: %d\n",res,comparisons);
 }
